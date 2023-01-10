819ce7dbb25b9d99202c2e39153013519bbea0d7
AndreeaLucaci <andreeateodoralucaci@gmail.com>
2023-01-10 22:16:10 +0200

    Add project files.

---
+++ Lucaci_Andreea_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Lucaci_Andreea_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
